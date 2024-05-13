70b236704c8fcc78343f0817386607bbf1ad84a6
FJMN0411 <rstoyo.fujii.maina@gmail.com>
2024-05-13 18:26:13 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :date
   belongs_to :user
   has_one_attached :image
 
@@ -10,5 +16,12 @@ class Item < ApplicationRecord
   validates :category_id, presence: true
   validates :condition_id, presence: true
   validates :cost_id, presence: true
+  alidates :area_id, presence: true
   alidates :date_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :date_id, numericality: { other_than: 1, message: "can't be blank" }
 end
