2ed4e43dc1ca45fa5669db0e437facf9734d6515
FJMN0411 <rstoyo.fujii.maina@gmail.com>
2024-05-13 16:38:29 +0900

    ユーザー管理機能の修正_01

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "はかな入力で入力してください" }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "はかな入力で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "はかな入力で入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "はかな入力で入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" }
   validates :birth, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,6 +47,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
+      it 'birtfが空では登録できない' do
+        @user.birth = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth can't be blank")
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
@@ -65,7 +70,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
-      it 'passwordが5文字以下では登録できない' do
+      it 'passwordが6文字以下では登録できない' do
         @user.password = '00000'
         @user.password_confirmation = '00000'
         @user.valid?
@@ -75,7 +80,42 @@ RSpec.describe User, type: :model do
         @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      end
+      it '英字のみのパスワードでは登録できない' do
+        user = User.new(password: 'password')
+        user.valid?
+        expect(user.errors[:password]).to include('は半角英数字混合で入力してください')
+      end
+      it '数字のみのパスワードでは登録できない' do
+        user = User.new(password: '123456')
+        user.valid?
+        expect(user.errors[:password]).to include('は半角英数字混合で入力してください')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        user = User.new(password: 'パスワード１２３')
+        user.valid?
+        expect(user.errors[:password]).to include('は半角英数字混合で入力してください')
+      end
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        user = User.new(first_name: 'name')
+        user.valid?
+        expect(user.errors[:first_name]).to include('はかな入力で入力してください')
+      end
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        user = User.new(last_name: 'name')
+        user.valid?
+        expect(user.errors[:last_name]).to include('はかな入力で入力してください')
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        user = User.new(first_name_kana: '名前')
+        user.valid?
+        expect(user.errors[:first_name_kana]).to include('はカタカナで入力してください')
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        user = User.new(last_name_kana: '名前')
+        user.valid?
+        expect(user.errors[:last_name_kana]).to include('はカタカナで入力してください')
       end
     end
   end
